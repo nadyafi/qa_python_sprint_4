5b77ad9f0af6558dc70d99f7fbdb3b5b52d96a73
Nadya Filippova <nadya.fi.pro@yandex.ru>
2024-03-26 22:51:38 +0300

    test for BooksCollector

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # qa_python
+Для методов add_new_book, set_book_genre, get_books_with_specific_genre реализованы позитивные и негативные сценарии проверки.
+Для методов get_book_genre, get_books_genre, get_books_for_children, add_book_in_favorites, delete_book_from_favorites, get_list_of_favorites_books реализованы позитивные тесты.
+tests.py::TestBooksCollector::test_add_new_book_success PASSED                                                                                            [  7%]
+tests.py::TestBooksCollector::test_add_new_book_empty_name PASSED                                                                                         [ 14%]
+tests.py::TestBooksCollector::test_set_books_genre_success PASSED                                                                                         [ 21%]
+tests.py::TestBooksCollector::test_set_book_genre_invalid_genre PASSED                                                                                    [ 28%]
+tests.py::TestBooksCollector::test_get_book_genre_existing_book PASSED                                                                                    [ 35%]
+tests.py::TestBooksCollector::test_get_books_with_specific_genre_success[Book_4-\u0414\u0435\u0442\u0435\u043a\u0442\u0438\u0432\u044b] PASSED            [ 42%]
+tests.py::TestBooksCollector::test_get_books_with_specific_genre_success[Book_5-\u0414\u0435\u0442\u0435\u043a\u0442\u0438\u0432\u044b] PASSED            [ 50%]
+tests.py::TestBooksCollector::test_get_books_with_specific_genre_non_existing_genre PASSED                                                                [ 57%]
+tests.py::TestBooksCollector::test_get_books_genre_success PASSED                                                                                         [ 64%]
+tests.py::TestBooksCollector::test_get_books_for_children_success PASSED                                                                                  [ 71%]
+tests.py::TestBooksCollector::test_add_book_in_favorites_success PASSED                                                                                   [ 78%]
+tests.py::TestBooksCollector::test_delete_book_from_favorites_success PASSED                                                                              [ 85%]
+tests.py::TestBooksCollector::test_get_list_of_favorites_books_success[Book_10] PASSED                                                                    [ 92%]
+tests.py::TestBooksCollector::test_get_list_of_favorites_books_success[Book_11] PASSED                                                                    [100%]
+
+====================================================================== 14 passed in 0.01s =======================================================================
--- tests.py
+++ tests.py
@@ -1,24 +1,72 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    @pytest.fixture
+    def collector(self):
+        return BooksCollector()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_success(self, collector):
+        collector.add_new_book('Book_1')
+        assert 'Book_1' in collector.books_genre
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book_empty_name(self, collector):
+        collector.add_new_book('')
+        assert '' not in collector.books_genre
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_books_genre_success(self, collector):
+        collector.add_new_book('Book_1')
+        collector.set_book_genre('Book_1', 'Комедии')
+        assert collector.books_genre['Book_1'] == 'Комедии'
+
+    def test_set_book_genre_invalid_genre(self, collector):
+        collector.add_new_book('Book_2')
+        collector.set_book_genre('Book_2', 'Поэма')
+        assert collector.books_genre['Book_2'] == ''
+
+    def test_get_book_genre_existing_book(self, collector):
+        collector.add_new_book('Book_3')
+        collector.set_book_genre('Book_3', 'Мультфильмы')
+        assert collector.get_book_genre('Book_3') == 'Мультфильмы'
+
+    @pytest.mark.parametrize('book, genre', [['Book_4', 'Детективы'], ['Book_5', 'Детективы']])
+    def test_get_books_with_specific_genre_success(self, collector, book, genre):
+        collector.add_new_book(book)
+        collector.set_book_genre(book, genre)
+        books = collector.get_books_with_specific_genre(genre)
+        assert book in books
+
+    def test_get_books_with_specific_genre_non_existing_genre(self, collector):
+        collector.add_new_book('Book_6')
+        collector.set_book_genre('Book_6', 'Фантастика')
+        books = collector.get_books_with_specific_genre('Ужасы')
+        assert books == []
+
+    def test_get_books_genre_success(self, collector):
+        assert collector.get_books_genre() == {}
+
+    def test_get_books_for_children_success(self, collector):
+        collector.add_new_book('Book_7')
+        collector.set_book_genre('Book_7', 'Мультфильмы')
+        children = collector.get_books_for_children()
+        assert 'Book_7' in children
+
+    def test_add_book_in_favorites_success(self, collector):
+        collector.add_new_book('Book_8')
+        collector.add_book_in_favorites('Book_8')
+        assert 'Book_8' in collector.favorites
+
+    def test_delete_book_from_favorites_success(self, collector):
+        collector.add_new_book('Book_9')
+        collector.add_book_in_favorites('Book_9')
+        collector.delete_book_from_favorites('Book_9')
+        assert 'Book_9' not in collector.favorites
+
+    @pytest.mark.parametrize('name', ['Book_10', 'Book_11'])
+    def test_get_list_of_favorites_books_success(self, collector, name):
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        favorites_list = collector.get_list_of_favorites_books()
+        assert name in favorites_list
